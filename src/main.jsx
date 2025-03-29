a3a30787ca716345550c29036941d6df5fb2500c
Yusuf <yusufbootwala83@gmail.com>
2025-03-29 10:34:17 +0530

    updated main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import viteLogo from '/vite.svg'
 import { BrowserRouter } from 'react-router-dom';
 import CoinContextProvider from './context/CoinContext.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx'; // Import ThemeProvider
